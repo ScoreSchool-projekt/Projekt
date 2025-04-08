eb849d6dd099f8095b5fff287f1ae6a8a6bab527
Dominika <180917293+ScoreSchool-projekt@users.noreply.github.com>
2025-04-08 18:08:51 +0200

    commit

--- Dokumentáció/Adatbázis/proba_lekerdezesek.sql
+++
@@ -1,6 +0,0 @@
-SELECT cs.csapatneve, c.pontok
-FROM csapat cs
-INNER JOIN csoport c ON cs.id = c.csapatid
-INNER JOIN torna t ON cs.tornaid = t.id
-WHERE t.tornaneve = "Falcsik Ferenc Emléktorna"
-ORDER BY `c`.`pontok` DESC;
